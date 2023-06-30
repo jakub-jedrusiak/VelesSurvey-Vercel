87a32679d696ce7b81dc9a9057ec3fd38efdcfdb
Jakub Jędrusiak <kuba23031999@gmail.com>
2023-06-30 19:28:55 +0200

    change build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,18 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+#!/bin/bash
+
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python -m venv venv
+source venv/bin/activate
+
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
