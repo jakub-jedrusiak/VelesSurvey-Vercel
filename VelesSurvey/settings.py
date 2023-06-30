2df3e8952f31cfd818246650653c79269525d52c
Jakub Jędrusiak <kuba23031999@gmail.com>
2023-06-30 20:16:56 +0200

    default DOMAIN_NAME

--- VelesSurvey/settings.py
+++ VelesSurvey/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", config("DOMAIN_NAME")]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1", config("DOMAIN_NAME", default="")]
 
 
 # Application definition
