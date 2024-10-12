cccec5345d17f7007ade6a4afe324b8ac3339e27
Jakub Jędrusiak <kuba23031999@gmail.com>
2024-10-12 14:18:11 +0200

    add reCAPTCHA handling

--- VelesSurvey/views.py
+++ VelesSurvey/views.py
@@ -1,6 +1,7 @@
 """Views for the VelesSurvey app."""
 
 import json
+import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
@@ -16,7 +17,14 @@ def index(request):
 
 def fill_survey(request, survey_id):
     """Render the survey form."""
-    return render(request, "fill_survey.html", {"survey_id": survey_id})
+    return render(
+        request,
+        "fill_survey.html",
+        {
+            "survey_id": survey_id,
+            "RECAPTCHA_SITE_KEY": config("RECAPTCHA_SITE_KEY", cast=str, default=""),
+        },
+    )
 
 
 @ensure_csrf_cookie
@@ -26,6 +34,20 @@ def submit_survey(request, survey_id):
         data_raw = json.loads(request.body)
         data = {"id": data_raw.pop("id")}
         data.update(data_raw)
+
+        # Handle reCAPTCHA
+        recaptcha_token = data.pop("g-recaptcha-token", None)
+        if recaptcha_token is not None:
+            recaptcha_response = requests.post(
+                "https://www.google.com/recaptcha/api/siteverify",
+                data={
+                    "secret": config("RECAPTCHA_SECRET_KEY", cast=str, default=""),
+                    "response": recaptcha_token,
+                },
+                timeout=5,
+            )
+            recaptcha_response = recaptcha_response.json()
+            data["g_recaptcha_score"] = recaptcha_response.get("score")
 
         uri = str(config("MONGODB_URI"))
         client = MongoClient(uri, server_api=ServerApi("1"))
